a084b72c59771841a0144009960fdc05d9ef66a7
Arisha Ansari <ansariarisha001@gmail.com>
2026-06-27 16:08:51 +0500

    URL

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://ecommerce-fullstack-design-khaki-tau.vercel.app'],
   credentials: true,
 }));
 app.use(express.json());
